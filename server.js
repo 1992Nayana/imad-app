828dfecb2347b946c8e2f3adc7edda77883eba7d
1992Nayana <30650856+1992Nayana@users.noreply.github.com>
2017-08-09 18:14:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ var config =
 var app = express();
 app.use(morgan('combined'));
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res) {
-    
     pool.query('SELECT * FROM test', function(err,result) {
        if(err) {
            res.status(500).send(err.toString());
